cbc9bbab0acbedf2697fcc4ec215669b1dfed650
Felix Linhart <linhartfelix0@gmail.com>
2021-01-26 00:12:34 +0100

    sheets are kind of working

---
+++ src/Frontend/scw/src/app/dataset/dataset.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import Handsontable from "handsontable";
+import 'handsontable/dist/handsontable.full.css';
+
+@Component({
+  selector: 'app-dataset',
+  templateUrl: './dataset.component.html',
+  styleUrls: ['./dataset.component.scss']
+})
+export class DatasetComponent implements OnInit {
+
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+}
