2718a5c40d42c474dab95043dc350e5ed5d41ea0
Nicolas Klement <nicolas.klement@gmail.com>
2021-01-26 22:00:17 +0100

    Add empty ScwSvc.Tests project

---
+++ src/Backend/ScwSvc.Tests/DynDbInteractorTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using ScwSvc.Models;
+
+namespace ScwSvc.Tests
+{
+    public class DynDbInteractorTests
+    {
+        private DbDynContext _dynDb;
+
+        [OneTimeSetUp]
+        public void SetupOnce()
+        {
+            _dynDb = new DbDynContext(new DbContextOptionsBuilder<DbDynContext>().UseInMemoryDatabase("test").UseLazyLoadingProxies().Options);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
