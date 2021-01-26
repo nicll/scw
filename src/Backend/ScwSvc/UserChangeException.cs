5f9933e5d488c0a5a0ffe85541a70be946db72e5
Nicolas Klement <nicolas.klement@gmail.com>
2021-01-26 18:13:13 +0100

    Add UserChangeException

---
+++ src/Backend/ScwSvc/UserChangeException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ScwSvc
+{
+    public class UserChangeException : Exception
+    {
+        public Guid UserId { get; init; }
+
+        public object OldValue { get; init; }
+
+        public object NewValue { get; init; }
+
+        public UserChangeException() : base()
+        {
+        }
+
+        public UserChangeException(string message) : base(message)
+        {
+        }
+
+        public UserChangeException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
