91e6cb9982aef0bd21d3eafae2910a6fc9bfbe71
Felix Linhart <linhartfelix0@gmail.com>
2021-01-27 16:06:25 +0100

    added several features, including a context menu for viewing and deleting, resizable columns, adjustable columns and rows and much more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import { ProductService } from './productservice';
 import { Product } from './product';
-import { LazyLoadEvent } from 'primeng/api';
+import {LazyLoadEvent, MenuItem} from 'primeng/api';
 import { SelectItem } from 'primeng/api';
 import {MessageService} from 'primeng/api';
 import {jsPDF} from 'jspdf';
@@ -24,32 +24,60 @@ export class AppComponent {
     cols: any[];
     products2: Product[];
 
+    selectedProduct: Product;
+
+    loading: boolean;
+
     statuses: SelectItem[];
 
     clonedProducts: { [s: string]: Product; } = {};
 
     exportColumns: any[];
+    items: MenuItem[];
+
 
 
     constructor(private productService: ProductService, private messageService: MessageService) { }
 
     ngOnInit() {
+        this.loading = true;
+        setTimeout(() => {
+            this.productService.getProductsSmall().then(data => this.products1 = data);
+            this.productService.getProductsSmall().then(data => this.products2 = data);
+            this.loading = false;
+        }, 1000);
         this.cols = [
+            { field: 'Re Order Column', header: 'Re Order Column' },
             { field: 'Code', header: 'Code' },
             { field: 'Name', header: 'Name' },
             { field: 'Category', header: 'Category' },
             { field: 'Quantity', header: 'Quantity' }
         ];
 
-         this.productService.getProductsSmall().then(data => this.products1 = data);
+        // this.productService.getProductsSmall().then(data => this.products1 = data);
+        // this.productService.getProductsSmall().then(data => this.products2 = data);
         console.log(localStorage.getItem('product'));
         // this.products1 = localStorage.getItem('product');
         // this.products2 = localStorage.getItem('product');
-         this.productService.getProductsSmall().then(data => this.products2 = data);
+
 
         this.statuses = [{label: 'In Stock', value: 'INSTOCK'}, {label: 'Low Stock', value: 'LOWSTOCK'},{label: 'Out of Stock', value: 'OUTOFSTOCK'}];
 
         this.exportColumns = this.statuses.map(col => ({title: col.label, dataKey: col.value}));
+
+        this.items = [
+            {label: 'View', icon: 'pi pi-fw pi-search', command: () => this.viewProduct(this.selectedProduct)},
+            {label: 'Delete', icon: 'pi pi-fw pi-times', command: () => this.deleteProduct(this.selectedProduct)}
+        ];
+    }
+    viewProduct(product: Product) {
+        this.messageService.add({severity: 'info', summary: 'Product Selected', detail: product.name });
+    }
+
+    deleteProduct(product: Product) {
+        this.products1 = this.products1.filter((p) => p.id !== product.id);
+        this.messageService.add({severity: 'info', summary: 'Product Deleted', detail: product.name});
+        this.selectedProduct = null;
     }
 
     onRowEditInit(product: Product) {
