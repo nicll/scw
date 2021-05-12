a1592b7f7c8df8a18e3a889e592fa9d359cbb4d9
Nicolas Klement <nicolas.klement@gmail.com>
2021-05-12 21:58:56 +0200

    Add basic postgraphile wrapper webserver

---
+++ src/PostGraphile/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const { postgraphile } = require("postgraphile");
+
+const app = express();
+
+app.use(
+    postgraphile(
+        "postgres://" + process.env.SCW1_DBUSER_DYN + ":" + process.env.SCW1_DBPASS_DYN + "@localhost:5432/scw",
+        "scw1_dyn",
+        {
+            watchPg: true,
+            graphiql: true,
+            enhanceGraphiql: true,
+            subscriptions: true,
+            ownerConnectionString: "postgres://" + process.env.SCW1_DBUSER_SYS + ":" + process.env.SCW1_DBPASS_SYS + "@localhost:5432/scw"
+        }
+    )
+);
+
+app.listen(3000);
