4eec09e8b422eabf18f0effb162c6a53455a13aa
Felix Desktop <linhartfelix0@gmail.com>
2021-10-19 18:28:19 +0200

    kept working on import function, almost works now

--- src/Frontend/scw/src/app/dataset/dataset.component.ts
+++ src/Frontend/scw/src/app/dataset/dataset.component.ts
@@ -246,16 +246,39 @@ export class DatasetComponent implements AfterViewInit, OnInit {
 
         /* save data */
         data = xlsx.utils.sheet_to_json(ws);
-        console.log("data" + data);
-
-        /*
-        * convert object to array of column names
-        * */
-        //TODO
+        console.log("data" + JSON.stringify(data[0]));
 
         //call the api
-        this.postDataSet(Object.keys(data[0]), ws)
+        console.log("worksheetname" + wsname);
+        let x;
 
+        for(x = 0; x < Object.keys(data[0]).length; x++){
+          this.completedColumns[x] = new Column(Object.keys(data[0])[x], "String", true)
+        }
+
+        const map1 = new Map();
+        for (const [key, value] of Object.entries(data[0])) {
+          map1.set(key, value);
+        }
+
+       // data[0].forEach(element => this.completedColumns[])
+
+    /*    console.log(JSON.stringify(wb.SheetNames))
+
+        console.log(JSON.stringify(this.completedColumns[0]))*/
+
+
+        //this.postDataSet(this.completedColumns, wsname)
+        //let element;
+        //this.user.GetDataSets().subscribe(y => console.log(y[y.length-1].tableRefId))
+        //@ts-ignore
+        this.user.GetDataSets().subscribe(y => this.apollo.Insert(y[y.length-1].tableRefId, map1).subscribe())
+       // console.log(JSON.stringify(element))
+
+
+
+
+        // console.log(wsname);
 
       };
       reader.readAsBinaryString(event.files[0]);
