23cc03c37faeafecdb7014808bb1e9f2a3cad32e
Nicolas Klement <nicolas.klement@gmail.com>
2022-01-10 01:13:06 +0100

    Add more tests

--- src/Backend/ScwSvc.Tests.Unit/LogOperationsTests.cs
+++ src/Backend/ScwSvc.Tests.Unit/LogOperationsTests.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using ScwSvc.DataAccess.Impl;
+using ScwSvc.Models;
 using ScwSvc.Operations.Impl;
 using ScwSvc.Operations.Interfaces;
 
@@ -25,8 +27,98 @@ public class LogOperationsTests
     }
 
     [Test, Order(1)]
-    public async Task Test()
+    public void LogUserLogEvent_ValidEntry_NoThrow()
     {
+        Assert.DoesNotThrowAsync(async () =>
+            await _userOp.LogUserEvent(new UserLogEvent() { Id = Guid.NewGuid(), Timestamp = DateTime.UtcNow, UserAction = UserLogEventType.Login })
+        );
+    }
 
+    [Test, Order(2)]
+    public void LogLookupLogEvent_ValidEntry_NoThrow()
+    {
+        Assert.DoesNotThrowAsync(async () =>
+            await _userOp.LogLookupEvent(new LookupLogEvent() { Id = Guid.NewGuid(), Timestamp = DateTime.UtcNow })
+        );
+    }
+
+    [Test, Order(3)]
+    public void LogTableDefinitionLogEvent_ValidEntry_NoThrow()
+    {
+        Assert.DoesNotThrowAsync(async () =>
+            await _tableOp.LogTableEvent(new TableDefinitionLogEvent() { Id = Guid.NewGuid(), Timestamp = DateTime.UtcNow, TableAction = TableDefinitionLogEventType.CreateTable })
+        );
+    }
+
+    [Test, Order(4)]
+    public void LogTableCollaboratorLogEvent_ValidEntry_NoThrow()
+    {
+        Assert.DoesNotThrowAsync(async () =>
+            await _tableOp.LogTableEvent(new TableCollaboratorLogEvent() { Id = Guid.NewGuid(), Timestamp = DateTime.UtcNow, CollaboratorAction = TableCollaboratorLogEventType.AddCollaborator })
+        );
+    }
+
+    [Test, Order(5)]
+    public async Task GetLogEvents_Unfiltered_Returns4Events()
+    {
+        var logEvents = await _userOp.GetLogEvents();
+
+        Assert.AreEqual(4, logEvents.Count);
+    }
+
+    [Test, Order(6)]
+    public async Task GetLogEvents_UserActions_Returns1Event()
+    {
+        var logEvents = await _userOp.GetLogEvents(typeFilter: LogEventType.User);
+
+        Assert.AreEqual(1, logEvents.Count);
+    }
+
+    [Test, Order(7)]
+    public async Task GetLogEvents_LookupActions_Returns1Event()
+    {
+        var logEvents = await _userOp.GetLogEvents(typeFilter: LogEventType.Lookup);
+
+        Assert.AreEqual(1, logEvents.Count);
+    }
+
+    [Test, Order(8)]
+    public async Task GetLogEvents_TableActions_Returns2Events()
+    {
+        var logEvents = await _userOp.GetLogEvents(typeFilter: LogEventType.Table);
+
+        Assert.AreEqual(2, logEvents.Count);
+    }
+
+    [Test, Order(9)]
+    public async Task GetLogEvents_TableDefinitionActions_Returns1Event()
+    {
+        var logEvents = await _userOp.GetLogEvents(typeFilter: LogEventType.TableDefinition);
+
+        Assert.AreEqual(1, logEvents.Count);
+    }
+
+    [Test, Order(10)]
+    public async Task GetLogEvents_TableActions_Returns1Event()
+    {
+        var logEvents = await _userOp.GetLogEvents(typeFilter: LogEventType.TableCollaboration);
+
+        Assert.AreEqual(1, logEvents.Count);
+    }
+
+    [Test, Order(11)]
+    public async Task GetLogEvents_DateOutOfRange_Returns0Events()
+    {
+        var logEvents = await _userOp.GetLogEvents(dateFilter: (DateTime.UnixEpoch, DateTime.UnixEpoch));
+
+        Assert.AreEqual(0, logEvents.Count);
+    }
+
+    [Test, Order(12)]
+    public async Task GetLogEvents_CatchAllFilters_Returns4Events()
+    {
+        var logEvents = await _userOp.GetLogEvents(typeFilter: LogEventType.All, dateFilter: (DateTime.MinValue, DateTime.MaxValue));
+
+        Assert.AreEqual(4, logEvents.Count);
     }
 }
--- src/Backend/ScwSvc.Tests.Unit/TableOperationsTests.cs
+++ src/Backend/ScwSvc.Tests.Unit/TableOperationsTests.cs
@@ -1,7 +1,9 @@
-﻿using System.Threading.Tasks;
+﻿using System;
 using Microsoft.EntityFrameworkCore;
 using NUnit.Framework;
 using ScwSvc.DataAccess.Impl;
+using ScwSvc.Exceptions;
+using ScwSvc.Models;
 using ScwSvc.Operations.Impl;
 using ScwSvc.Operations.Interfaces;
 
@@ -9,19 +11,141 @@ namespace ScwSvc.Tests.Unit;
 
 public class TableOperationsTests
 {
+    private DbSysContext _sysDb;
+    private DbDynContext _dynDb;
     private ITableOperations _tableOp;
 
     [OneTimeSetUp]
     public void SetupOnce()
     {
+        _sysDb = new DbSysContext(new DbContextOptionsBuilder<DbSysContext>().UseInMemoryDatabase(nameof(TableOperationsTests)).Options);
+        _dynDb = new DbDynContext(new DbContextOptionsBuilder<DbDynContext>().UseInMemoryDatabase(nameof(TableOperationsTests)).Options);
+
         _tableOp = new TableOperations(
-            new SysDbRepository(new(new DbContextOptionsBuilder<DbSysContext>().UseInMemoryDatabase(nameof(TableOperationsTests)).Options)),
-            new DynDbRepository(new(new DbContextOptionsBuilder<DbDynContext>().UseInMemoryDatabase(nameof(TableOperationsTests)).Options)));
+            new SysDbRepository(_sysDb),
+            new DynDbRepository(_dynDb));
     }
 
     [Test, Order(1)]
-    public async Task Test()
+    public void AddTable_InvalidTableType_TableMismatchException()
     {
+        var table = new Table
+        {
+            TableId = Guid.NewGuid(),
+            DisplayName = "asdf",
+            LookupName = Guid.NewGuid(),
+            OwnerUserId = Guid.NewGuid(),
+            TableType = (TableType)(-1)
+        };
 
+        Assert.ThrowsAsync<TableMismatchException>(async () => await _tableOp.AddTable(table));
+    }
+
+    [Test, Order(2)]
+    public void AddTable_InvalidTableId_TableDeclarationException()
+    {
+        var table = new Table
+        {
+            TableId = Guid.Empty,
+            DisplayName = "asdf",
+            LookupName = Guid.NewGuid(),
+            OwnerUserId = Guid.NewGuid(),
+            TableType = (TableType)1
+        };
+
+        Assert.ThrowsAsync<TableDeclarationException>(async () => await _tableOp.AddTable(table));
+    }
+
+    [Test, Order(3)]
+    public void AddTable_InvalidLookupName_TableDeclarationException()
+    {
+        var table = new Table
+        {
+            TableId = Guid.NewGuid(),
+            DisplayName = "asdf",
+            LookupName = Guid.Empty,
+            OwnerUserId = Guid.NewGuid(),
+            TableType = (TableType)1
+        };
+
+        Assert.ThrowsAsync<TableDeclarationException>(async () => await _tableOp.AddTable(table));
+    }
+
+    [Test, Order(4)]
+    public void AddTable_InvalidOwnerId_TableDeclarationException()
+    {
+        var table = new Table
+        {
+            TableId = Guid.NewGuid(),
+            DisplayName = "asdf",
+            LookupName = Guid.NewGuid(),
+            OwnerUserId = Guid.Empty,
+            TableType = (TableType)1
+        };
+
+        Assert.ThrowsAsync<TableDeclarationException>(async () => await _tableOp.AddTable(table));
+    }
+
+    [Test, Order(5)]
+    public void AddTable_InvalidDisplayName_TableDeclarationException()
+    {
+        var table = new Table
+        {
+            TableId = Guid.NewGuid(),
+            DisplayName = "",
+            LookupName = Guid.NewGuid(),
+            OwnerUserId = Guid.NewGuid(),
+            TableType = (TableType)1
+        };
+
+        Assert.ThrowsAsync<TableDeclarationException>(async () => await _tableOp.AddTable(table));
+    }
+
+    [Test, Order(6)]
+    public void AddTable_SheetColumns_TableColumnException()
+    {
+        var table = new Table
+        {
+            TableId = Guid.NewGuid(),
+            DisplayName = "asdf",
+            LookupName = Guid.NewGuid(),
+            OwnerUserId = Guid.NewGuid(),
+            TableType = TableType.Sheet,
+            Columns = new DataSetColumn[1]
+        };
+
+        Assert.ThrowsAsync<TableColumnException>(async () => await _tableOp.AddTable(table));
+    }
+
+    [Test, Order(7)]
+    public void AddTable_DataSetNoColumns_TableColumnException()
+    {
+        var table = new Table
+        {
+            TableId = Guid.NewGuid(),
+            DisplayName = "asdf",
+            LookupName = Guid.NewGuid(),
+            OwnerUserId = Guid.NewGuid(),
+            TableType = TableType.DataSet,
+            Columns = new DataSetColumn[0]
+        };
+
+        Assert.ThrowsAsync<TableColumnException>(async () => await _tableOp.AddTable(table));
+    }
+
+    [Test, Order(8)]
+    public void AddTable_DataSetManyColumns_TableColumnException()
+    {
+        var table = new Table
+        {
+            TableId = Guid.NewGuid(),
+            DisplayName = "asdf",
+            LookupName = Guid.NewGuid(),
+            OwnerUserId = Guid.NewGuid(),
+            TableType = TableType.DataSet,
+            Columns = new DataSetColumn[_tableOp.MaxNumberOfColumns + 1]
+        };
+
+        Assert.ThrowsAsync<TableColumnException>(async () => await _tableOp.AddTable(table));
     }
 }
